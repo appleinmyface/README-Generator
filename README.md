ab9099d34e0cfae53bfa93bca11e15b5cb7e0db8
Eric Hernandez <derickhh.05@gmail.com>
2024-07-23 10:22:41 -0400

    made all files and coded everything, the README is made once I run the command 'node index.js' then asks questions about what youd like to put in the README

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# README-Generator
-A program that can generate a README
+# README generator
+  
+  ![License](https://img.shields.io/badge/License-MIT-blue.svg)
+  
+  ## Description
+  
+  To Create a README File
+  
+  ## Table of Contents
+  
+  * [Installation](#installation)
+  * [Usage](#usage)
+  
+* [License](#license)
+
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation
+  
+  git clone blah blah blah
+  
+  ## Usage
+  
+  README is fun
+  
+  ## License
+
+This project is licensed under the MIT license.
+  
+  ## Contributing
+  
+  guidlines go here
+  
+  ## Tests
+  
+  to test do this and that
+  
+  ## Questions
+  
+  If you have any questions about the repo, open an issue or contact me directly at [derickhh.05@gmail.com](mailto:derickhh.05@gmail.com). You can find more of my work at [AppleInMyFace](https://github.com/AppleInMyFace).
+  
---
+++ index.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generate.js');
+
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:'
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide the installation instructions:'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide usage information:'
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide contribution guidelines:'
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:'
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None']
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username:'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:'
+  }
+];
+
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error('Error writing file:', err);
+    } else {
+      console.log('README.md generated successfully!');
+    }
+  });
+}
+
+function init() {
+  inquirer.prompt(questions).then((responses) => {
+    const markdown = generateMarkdown(responses);
+    writeToFile('README.md', markdown);
+  });
+}
+
+init();
